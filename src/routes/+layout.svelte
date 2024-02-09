f8f44affaa83c2ebf4d5e30b9e8df7ccc214293a
Denis <mironovd715@gmail.com>
2024-02-09 17:54:36 +0400

    init3

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<svelte:head>
+	<title>Главная</title>
+</svelte:head>
+
+<nav>
+	<div class="nav-container">
+		<div class="nav-links">
+			{#each nav as link}
+			{#if link.isShow}
+				<a href={link.href} class="link">{link.title}</a>
+			{/if}
+			{/each}
+		</div>
+	</div>
+</nav>
+
+<div class="contayner">
+	<div class="hone_rot"></div>
+	<div class="rot"></div>
+	<div class="cot">
+		<p class="h1">ВПК "Братишка"</p>
+		<p class="p1">город Ахтубинск, Астраханская область</p>
+	</div>
+</div>
+
+<div class="container">
+	<slot></slot>
+</div>
+
+<script>
+	const nav = [
+		{ 
+			title: 'Главная',
+			href: '/',
+			isShow: true
+		},
+		{ 
+			title: 'Занятия',
+			href: '/about',
+			isShow: true
+		},
+		{
+			title: 'Медиа',
+			href: '/media',
+			isShow: true
+		},
+		{
+			title: 'Достижения',
+			href: '/lessons',
+			isShow: true
+		},
+		{
+			title: 'Контакты',
+			href: '/new',
+			isShow: true
+		}
+	]
+</script>
+<style>
+	.container {
+		max-width: 1400px;
+		margin: 50px auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,78 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="virav">
+</div>
+
+<div class="opisanie">
+	<div class="spoiler_wrap">
+		<div class="spoiler_title">
+			Описание
+			<span class="spoiler_arrow">
+				<svg viewBox="-122.9 121.1 105.9 61.9">
+					<path d="M-63.2 180.3l43.5-43.5c1.7-1.7 2.7-4 2.7-6.5s-1-4.8-2.7-6.5c-1.7-1.7-4-2.7-6.5-2.7s-4.8 1-6.5 2.7L-69.9 161l-37.2-37.2c-1.7-1.7-4-2.7-6.5-2.7s-4.8 1-6.5 2.6c-1.9 1.8-2.8 4.2-2.8 6.6 0 2.3.9 4.6 2.6 6.5 11.4 11.5 41 41.2 43 43.3l.2.2c3.6 3.6 10.3 3.6 13.9 0z"></path>
+				</svg>
+			</span>
+		</div>
+		<div class="spoiler_content">
+			<h1 class="h2">Военно-патриотический клуб «БРАТИШКА»<h1>
+			<p class="p10">Проводится работа с молодежью, в том числе допризывной, по военно-патриотическому воспитанию, военно-прикладным видам спорта, военной начальной подготовке, активному и полезному времяпрепровождению.</p>
+			<p>Направления деятельности:</p>
+			<ul>
+				<li>Воздушно-десантная подготовка (работа при десантировании, прыжки с парашютом);</li>
+				<li>Огневая подготовка (оружейная культура, правила точной и меткой стрельбы, практические стрельбы);</li>
+				<li> Военно-штурмовой альпинизм (работа с альпинистским снаряжением, штурм здания, преодоление водных преград, скалолазание);</li>
+				<li>Общевойсковая подготовка (строевая, РХБЗ);</li>
+				<li>Тактико-специальная подготовка (разведка, ориентирование, выживание);</li>
+				<li>История России в лицах Спецназа (ратные подвиги обычных людей из Спецназа ВДВ, ГРУ, ФСБ);</li>
+				<li> Правовая подготовка (правовое информирование, порядок поступления в военные ВУЗы РФ);</li>
+				<li>Военно-тактические игры, соревнования, мероприятия;</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<h1 class="bit">Новости</h1>
+
+<section class="grid">
+	<article class="grid-item">
+		<div class="image">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="new1.jpg"/>
+		</div>
+		<div class="info">
+		</div>
+	</article>
+</section>
+<div class="blocck">
+	<article class="grid-item">
+		<div class="image">
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="new3.jpg"/>
+		</div>
+		<div class="info">
+		</div>
+	</article>
+</div>
+<div class="ffg">
+	<div class="container">
+		<div class="forms">
+			<div class="form login">
+			<span class="title">17 августа 2021 г</span>
+			</div>
+		</div>
+	</div>
+	<div class="blockk"> 
+		<p>Свой первый парашютный прыжок юные ахтубинские спортсмены совершили на аэродроме Средней Ахтубы: <a href="https://www.youtube.com/watch?v=DF4t35AxprI">ссылка</a>.</p>
+	</div>
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img src="Rectangle 1 (1).png" class="ss_1">
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img src="Rectangle 1.png" class="ss_1">
+</div>
+<footer>
+	<p class="odin">Предложить идеи?</p>
+	<p class="dva">Нашли ошибку?</p>
+	<p class="try">Разработчик: Миронов Денис Иванович</p>
+	<p class="try">Связаться с разработчиком:</p>
+	<ul>
+		<li>Электронная почта: mironovd715@gmail.com</li>
+	</ul>
+</footer>
